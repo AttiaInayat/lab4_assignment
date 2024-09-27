3434552dbc012a679e8f01cad2313675a0db9ec5
AttiaInayat <umarg6902@gmail.com>
2024-09-27 17:32:59 +0500

    on branch commit 2

--- Product_stubs.py
+++ Product_stubs.py
@@ -5,17 +5,7 @@ class Product:
       self.price = price
     
   def get_price(self, quantity):
-      if quantity<0:
-          raise ValueError('Quantity is not valid')
-      elif quantity<10: # for 0 to 9
-          discount = 0
-      elif quantity<100: # for 10 to 99
-          discount = 0.10
-      else: # for 100 or above
-          discount = 0.20
-
-      totalprice= quantity * self.price * (1 - discount)
-      return totalprice
+      pass
   
   def make_purchase(self, quantity):
       pass
